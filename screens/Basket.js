b2cfea9b0cb74942fd212e9a8cc4cc926ffd6921
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-11-29 08:57:22 -0300

    Adicionando detalhes da cesta e imagem

--- screens/Basket.js
+++ screens/Basket.js
@@ -1,5 +1,6 @@
-import { Image, StyleSheet, Text, Dimensions } from "react-native";
+import { Image, StyleSheet, Text, Dimensions, View } from "react-native";
 import headerImage from '../assets/topo.png'
+import logo from '../assets/logo.png'
 
 const width = Dimensions.get('screen').width;
 
@@ -8,6 +9,20 @@ export function Basket() {
         <>
             <Image source={headerImage} style={styles.headerImage} />
             <Text style={styles.title}>Detail of Basket</Text>
+
+            <View style={styles.basket}>
+                <Text style={styles.name}>Vegetable Basket</Text>
+                <View style={styles.farm}>
+                    <Image source={logo} style={styles.farmImage}/>
+                    <Text style={styles.farmName}>Jenny Jack Farm</Text>
+                </View>
+
+                <Text style={styles.description}>
+                    A basket with carefully selected
+                    products from the farm straight to your kitchen
+                </Text>
+                <Text style={styles.price}>R$ 40,00</Text>
+            </View>
         </>
 
     );
@@ -19,14 +34,50 @@ const styles = StyleSheet.create({
         height: 578 / 768 * width // Conta para calcular
 
     },
-    title:{
-        position:'absolute',
-        textAlign:"center",
-        width:'100%',
-        fontSize:16,
-        lineHeight:26,
-        color:'white',
-        fontWeight:'bold',
-        padding:16
+    title: {
+        position: 'absolute',
+        textAlign: "center",
+        width: '100%',
+        fontSize: 16,
+        lineHeight: 26,
+        color: 'white',
+        fontWeight: 'bold',
+        padding: 16
+    },
+    basket: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    name: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: '#464646',
+        fontWeight: 'bold'
+    },
+    farm:{
+        flexDirection:'row',
+        paddingVertical:12
+    },
+    farmImage:{
+        width:32,
+        height:32
+    },  
+    farmName: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft:12
+
+    },
+    description: {
+        color: '#A3A3A3',
+        fontSize: 16,
+        lineHeight: 26
+    },
+    price: {
+        color: '#2A9F85',
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 })
